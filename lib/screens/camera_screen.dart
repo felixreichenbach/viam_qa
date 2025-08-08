f090ff1571d475b3954f63b80541868621882e94
felixreichenbach <felix.reichenbach@viam.com>
2025-08-08 09:32:56 +0200

    renamed

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:viam_qa/utils.dart';
+import 'package:viam_qa/classification.dart';
 
 // A screen that allows users to take a picture using a given camera.
 class TakePictureScreen extends StatefulWidget {
