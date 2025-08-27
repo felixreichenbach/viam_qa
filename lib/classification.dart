07a5e68a9123a43b632f69ddd0bdedd643cf18eb
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2025-08-27 12:31:55 +0200

    Improvements (#8)
    * App behaviour improvements
    
    * status message display improvements
    
    * Model updated to 2025-08-27T12-07-40

--- lib/classification.dart
+++ lib/classification.dart
@@ -1,5 +1,4 @@
 import 'package:tflite_flutter/tflite_flutter.dart';
-import 'dart:io';
 import 'dart:typed_data';
 import 'package:image/image.dart' as img;
 import 'package:flutter/services.dart' show rootBundle;
--- lib/viam.dart
+++ lib/viam.dart
@@ -10,39 +10,35 @@ Future<void> uploadTabularData(
   String userClassification,
   List<Map<String, dynamic>> inference,
 ) async {
-  try {
-    _viam = await Viam.withApiKey(
-      dotenv.env['API_KEY_ID'] ?? '',
-      dotenv.env['API_KEY'] ?? '',
-    );
+  _viam = await Viam.withApiKey(
+    dotenv.env['API_KEY_ID'] ?? '',
+    dotenv.env['API_KEY'] ?? '',
+  );
 
-    final List<Map<String, dynamic>> tabularData;
-    tabularData = [
-      {
-        'readings': {
-          'classifications': inference,
-          'user_classification': userClassification,
-          'image_id': imgId,
-        },
+  final List<Map<String, dynamic>> tabularData;
+  tabularData = [
+    {
+      'readings': {
+        'classifications': inference,
+        'user_classification': userClassification,
+        'image_id': imgId,
       },
-    ];
+    },
+  ];
 
-    final List<(DateTime, DateTime)> dataRequestTimes = [
-      (DateTime.now(), DateTime.now()),
-    ];
+  final List<(DateTime, DateTime)> dataRequestTimes = [
+    (DateTime.now(), DateTime.now()),
+  ];
 
-    final _ = await _viam.dataClient.tabularDataCaptureUpload(
-      tabularData,
-      '177e6f10-4505-42fe-bf81-bb2867b680a0',
-      componentType: "rdk:component:sensor",
-      componentName: "qa-app",
-      methodName: "Readings",
-      dataRequestTimes: dataRequestTimes,
-      tags: ["test"],
-    );
-  } catch (e) {
-    print(e);
-  }
+  final _ = await _viam.dataClient.tabularDataCaptureUpload(
+    tabularData,
+    '177e6f10-4505-42fe-bf81-bb2867b680a0',
+    componentType: "rdk:component:sensor",
+    componentName: "qa-app",
+    methodName: "Readings",
+    dataRequestTimes: dataRequestTimes,
+    tags: ["test"],
+  );
 }
 
 Future<String> uploadImageData(
@@ -60,28 +56,25 @@ Future<String> uploadImageData(
     });
     mlRating = inference.first['label'] ?? '';
   }
-  try {
-    _viam = await Viam.withApiKey(
-      dotenv.env['API_KEY_ID'] ?? '',
-      dotenv.env['API_KEY'] ?? '',
-    );
 
-    (DateTime, DateTime) dataRequestTimes = (DateTime.now(), DateTime.now());
+  _viam = await Viam.withApiKey(
+    dotenv.env['API_KEY_ID'] ?? '',
+    dotenv.env['API_KEY'] ?? '',
+  );
 
-    final result = await _viam.dataClient.binaryDataCaptureUpload(
-      imageBytes,
-      '177e6f10-4505-42fe-bf81-bb2867b680a0',
-      componentType: "rdk:component:camera",
-      componentName: "camera",
-      methodName: "ReadImage",
-      '.jpg',
-      dataRequestTimes: dataRequestTimes,
-      tags: ["flutter", userRating, mlRating],
-    );
-    print('Image data upload result: $result');
-    return result;
-  } catch (e) {
-    print(e);
-  }
-  return '';
+  (DateTime, DateTime) dataRequestTimes = (DateTime.now(), DateTime.now());
+
+  final result = await _viam.dataClient.binaryDataCaptureUpload(
+    imageBytes,
+    '177e6f10-4505-42fe-bf81-bb2867b680a0',
+    componentType: "rdk:component:camera",
+    componentName: "camera",
+    methodName: "ReadImage",
+    '.jpg',
+    dataRequestTimes: dataRequestTimes,
+    tags: ["flutter", userRating, mlRating],
+    datasetIds: ["68aebb20d839e9020c3f843c"],
+  );
+  print('Image data upload result: $result');
+  return result;
 }
